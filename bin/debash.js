d80488971566579c01cba418a2eb26b633183ae3
zlatov <zlatov@ya.ru>
2026-07-30 10:31:35 +0300

    Add CLI skeleton (stage 0)

---
+++ bin/debash.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const line = "─".repeat(42);
+
+console.log();
+console.log(line);
+console.log();
+console.log("  Debash");
+console.log();
+console.log(line);
+console.log();
